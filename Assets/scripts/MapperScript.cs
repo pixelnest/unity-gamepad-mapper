759af48dbcdb1027fcef101c2ad67c327138955e
Damien Mayance <dmayance@gmail.com>
2015-10-09 19:01:04 +0200

    Binding for buttons is OK

--- Assets/scripts/MapperScript.cs
+++ Assets/scripts/MapperScript.cs
@@ -13,17 +13,26 @@ public class MapperScript : MonoBehaviour
   [Header("UI")]
   public MapperUIScript ui;
 
-  private string deviceName;
+  private int joystickId;
+  private string joystickName;
+
+  private int bindingIndex;
+  private string bindingHandle;
+  private bool lookForAnalogs;
+  private string result;
 
   void Start()
   {
-    ui.SetDetectedDevices(Input.GetJoystickNames());
+    joystickId = -1;
+    bindingIndex = -1;
+
+    result = string.Empty;
   }
 
 
   void Update()
   {
-    if (string.IsNullOrEmpty(deviceName))
+    if (joystickId < 0)
     {
       UpdateNoDevice();
     }
@@ -35,27 +44,155 @@ public class MapperScript : MonoBehaviour
 
   private void UpdateNoDevice()
   {
+    ui.SetDetectedDevices(Input.GetJoystickNames());
+
     var key = CheckForKey();
     if (key != KeyCode.None)
     {
-      Debug.Log(key);
+      // Convert Joystick3Button42 to 3
+      try
+      {
+        joystickId = int.Parse(key.ToString().Replace("Joystick", "").Split(new string[] { "Button" }, System.StringSplitOptions.RemoveEmptyEntries)[0]);
+        joystickName = Input.GetJoystickNames()[joystickId - 1];
+
+        // Launch binding UI
+        ui.ShowMappings("#" + joystickId + " " + joystickName);
+
+        NextBinding();
+      }
+      catch (System.Exception)
+      {
+
+      }
     }
   }
 
   private void UpdateDeviceBinding()
   {
+    string binding = string.Empty;
 
+    // Skip or restart?
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      NextBinding();
+      return;
+    }
+
+    if (Input.GetKeyDown(KeyCode.R))
+    {
+      Application.LoadLevel(Application.loadedLevelName);
+      return;
+    }
+
+    // Axes?
+    if (lookForAnalogs)
+    {
+      var analogs = CheckForAnalogs();
+      if (analogs.Count > 0)
+      {
+        binding = string.Empty;
+
+        foreach (var axes in analogs)
+        {
+          binding += "Joystick" + axes.Key + "Analog" + axes.Value + " ";
+        }
+      }
+    }
+    // Key?
+    else
+    {
+      var key = CheckForKey();
+      if (key != KeyCode.None)
+      {
+        binding = key.ToString();
+      }
+    }
+
+    if (string.IsNullOrEmpty(binding) == false)
+    {
+      ui.SetBindingValue(bindingIndex, binding);
+
+      result += string.Format("{0}\t=\t{1}\r\n", bindingHandle, binding);
+
+      NextBinding();
+    }
   }
 
-  private KeyCode CheckForKey(string joystickId = "")
+  private void NextBinding()
+  {
+    bindingIndex++;
+    lookForAnalogs = false;
+
+    switch (bindingIndex)
+    {
+      case 0:
+        bindingHandle = "A / Cross";
+        break;
+      case 1:
+        bindingHandle = "B / Circle";
+        break;
+      case 2:
+        bindingHandle = "X / Square";
+        break;
+      case 3:
+        bindingHandle = "Y / Triangle";
+        break;
+      case 4:
+        bindingHandle = "Start";
+        break;
+      case 5:
+        bindingHandle = "Back / Select";
+        break;
+      case 6:
+        bindingHandle = "RT / R2";
+        lookForAnalogs = true;
+        break;
+      case 7:
+        bindingHandle = "LT / L2";
+        lookForAnalogs = true;
+        break;
+      case 8:
+        bindingHandle = "RB / R1";
+        break;
+      case 9:
+        bindingHandle = "LB / L1";
+        break;
+      case 10:
+        bindingHandle = "DPad Up";
+        lookForAnalogs = true;
+        break;
+      case 11:
+        bindingHandle = "DPad Down";
+        lookForAnalogs = true;
+        break;
+      case 12:
+        bindingHandle = "DPad Left";
+        lookForAnalogs = true;
+        break;
+      case 13:
+        bindingHandle = "DPad Right";
+        lookForAnalogs = true;
+        break;
+      case 14:
+        bindingHandle = "Left stick";
+        lookForAnalogs = true;
+        break;
+    }
+
+    ui.SetActiveBinding(bindingIndex, bindingHandle);
+  }
+
+  private KeyCode CheckForKey(string id = "")
   {
     foreach (KeyCode k in System.Enum.GetValues(typeof(KeyCode)))
     {
       // Get "JoystickNButtonX" keys but not "JoystickButtonX" keys
-      if (k.ToString().Contains("Joystick" + joystickId) && !k.ToString().Contains("JoystickButton"))
+      if (k.ToString().Contains("Joystick" + id) && !k.ToString().Contains("JoystickButton"))
       {
         if (Input.GetKeyDown(k))
         {
+          Debug.Log("Button input detected: " + k);
+
           return k;
         }
       }
@@ -72,12 +209,15 @@ public class MapperScript : MonoBehaviour
     {
       for (int a = 0; a <= JOYSTICK_ANALOG_COUNT; a++)
       {
-        float f = Input.GetAxis(string.Format("joystick {0} analog {1}", i, a));
+        string s = string.Format("joystick {0} analog {1}", i, a);
+        float f = Input.GetAxis(s);
 
         const float DEAD_ZONE = 0.2f;
 
         if (Mathf.Abs(f) > DEAD_ZONE)
         {
+          Debug.Log("Analog input detected: " + s + " = " + f);
+
           analogs.Add(new KeyValuePair<int, int>(i, a));
         }
       }
--- Assets/scripts/MapperUIScript.cs
+++ Assets/scripts/MapperUIScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
 
 /// <summary>
 /// Handles UI for the mapping
@@ -14,10 +15,26 @@ public class MapperUIScript : MonoBehaviour
   [Header("Misc")]
   public Text detectedDevicesLabel;
 
+  [Header("Bindings")]
+  public Text currentDeviceLabel;
+  public Text[] bindingsHandles;
+  public Text[] bindings;
+
+  private Text currentActiveBindingHandle, currentActiveBinding;
+
   void Start()
   {
     startPanel.SetActive(true);
     mappingPanel.SetActive(false);
+
+    foreach (var b in bindingsHandles)
+    {
+      b.gameObject.SetActive(false);
+    }
+    foreach (var b in bindings)
+    {
+      b.gameObject.SetActive(false);
+    }
   }
 
   /// <summary>
@@ -29,13 +46,52 @@ public class MapperUIScript : MonoBehaviour
     detectedDevicesLabel.text = string.Empty;
     for (int i = 0; i < devices.Length; i++)
     {
-      detectedDevicesLabel.text = devices[i] + (i < devices.Length - 1 ? ", " : "");
+      detectedDevicesLabel.text += devices[i] + (i < devices.Length - 1 ? ", " : "");
     }
   }
 
-  public void ShowMappings()
+  public void ShowMappings(string deviceId)
   {
     startPanel.SetActive(false);
     mappingPanel.SetActive(true);
+
+    currentDeviceLabel.text = deviceId;
   }
+
+  public void SetActiveBinding(int bindingIndex, string bindingHandle)
+  {
+    if (currentActiveBindingHandle != null)
+    {
+      currentActiveBindingHandle.color = Color.white;
+      currentActiveBindingHandle = null;
+    }
+    if (currentActiveBinding != null)
+    {
+      currentActiveBinding.color = Color.white;
+      currentActiveBinding = null;
+    }
+
+    if (bindingIndex < bindingsHandles.Length)
+    {
+      currentActiveBindingHandle = bindingsHandles[bindingIndex];
+      currentActiveBindingHandle.gameObject.SetActive(true);
+      currentActiveBindingHandle.color = Color.green;
+
+      currentActiveBinding = bindings[bindingIndex];
+      currentActiveBinding.gameObject.SetActive(true);
+      currentActiveBinding.color = Color.green;
+
+      bindingsHandles[bindingIndex].text = bindingHandle;
+    }
+  }
+
+  public void SetBindingValue(int i, string key)
+  {
+    if (i < bindings.Length)
+    {
+      bindings[i].text = key;
+    }
+  }
+
+
 }
